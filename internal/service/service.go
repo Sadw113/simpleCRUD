1c0eea4e4a40cecaf14d974f4480b787e7adffd1
sadw113 <ww_wcw133141@mail.ru>
2025-03-06 00:39:50 +0300

    first commit, need to refactor handlers

---
+++ internal/service/service.go
@@ -0,0 +1,89 @@
+package service
+
+import (
+	"encoding/json"
+	"simple-service/internal/dto"
+	"simple-service/internal/repo"
+	"simple-service/pkg/validator"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
+
+// Слой бизнес-логики. Тут должна быть основная логика сервиса
+
+// Service - интерфейс для бизнес-логики
+type Service interface {
+	CreateTask(ctx *fiber.Ctx) error
+	GetTaskByID(ctx *fiber.Ctx) error
+}
+
+type service struct {
+	repo repo.Repository
+	log  *zap.SugaredLogger
+}
+
+// NewService - конструктор сервиса
+func NewService(repo repo.Repository, logger *zap.SugaredLogger) Service {
+	return &service{
+		repo: repo,
+		log:  logger,
+	}
+}
+
+// CreateTask - обработчик запроса на создание задачи
+func (s *service) CreateTask(ctx *fiber.Ctx) error {
+	var req TaskRequest
+
+	// Десериализация JSON-запроса
+	if err := json.Unmarshal(ctx.Body(), &req); err != nil {
+		s.log.Error("Invalid request body", zap.Error(err))
+		return dto.BadResponseError(ctx, dto.FieldBadFormat, "Invalid request body")
+	}
+
+	// Валидация входных данных
+	if vErr := validator.Validate(ctx.Context(), req); vErr != nil {
+		return dto.BadResponseError(ctx, dto.FieldIncorrect, vErr.Error())
+	}
+
+	// Вставка задачи в БД через репозиторий
+	task := repo.Task{
+		Title:       req.Title,
+		Description: req.Description,
+	}
+	taskID, err := s.repo.CreateTask(ctx.Context(), task)
+	if err != nil {
+		s.log.Error("Failed to insert task", zap.Error(err))
+		return dto.InternalServerError(ctx)
+	}
+
+	// Формирование ответа
+	response := dto.Response{
+		Status: "success",
+		Data:   map[string]int{"task_id": taskID},
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(response)
+}
+
+func (s *service) GetTaskByID(ctx *fiber.Ctx) error {
+	id, err := strconv.Atoi(ctx.Params("id"))
+	if err != nil {
+		s.log.Error("Filed to parse id from request", zap.Error(err))
+		return dto.BadResponseError(ctx, dto.FieldBadFormat, "Invalid request body")
+	}
+
+	task, err := s.repo.GetTaskByID(ctx.Context(), uint32(id))
+	if err != nil {
+		s.log.Error("Failed to selected from DB Table", zap.Error(err))
+		return dto.InternalServerError(ctx)
+	}
+
+	response := dto.Response{
+		Status: "success",
+		Data:   task,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(response)
+}
